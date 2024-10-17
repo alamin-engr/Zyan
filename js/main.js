3c44f9d30deedc27d53d309b5a2a8e52cba15121
alamin-engr <engr.aamin999@gmail.com>
2024-10-18 01:11:28 +0600

    added

---
+++ js/main.js
@@ -0,0 +1,33 @@
+$(function () {
+
+    "use strict";
+
+
+
+
+    $('.select_2').select2({
+        placeholder: 'Select an option'
+    });
+    
+// venobox js
+$('.venobox').venobox();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
